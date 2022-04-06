95e49a44e514829fb3ea473d0210412399df937f
Yuliia <bondarenko047@gmail.com>
2022-04-06 22:37:22 +0300

    js les

---
+++ app.js
@@ -0,0 +1,26 @@
+// 1 Переменные
+
+const name = 'Yuliia'
+let lastName = 'Bondarenko' // string
+const age = 18 // number
+const isProgrammer = true // boolean
+
+const _ = 'private'
+const $ = 'some value'
+
+// const if = 'mfmkk' // err
+const withNum5 = '5'
+// const 5withNum = 5 // err
+
+// console.log(name, lastName)
+// console.log(isProgrammer)
+
+// 2 Мутирование
+
+console.log('Имя человека: ' + name + ' , а возраст человека: ' + age)
+console.log(age.toString())
+
+// видео просмотра время 48:45
+
+const firstName = prompt('Введите фамилию')
+alert(firstName + ' ' + name)

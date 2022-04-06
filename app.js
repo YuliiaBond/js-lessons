08ca07709ae38e8112010687e45452d8df79a685
Yuliia <bondarenko047@gmail.com>
2022-04-06 23:08:13 +0300

    1

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// 1 Переменные
+// 1 ПЕРЕМЕННЫЕ
 
-const name = 'Yuliia'
-let lastName = 'Bondarenko' // string
-const age = 18 // number
-const isProgrammer = true // boolean
+// const name = 'Yuliia'
+// let lastName = 'Bondarenko' // string
+// const age = 18 // number
+// const isProgrammer = true // boolean
 
 const _ = 'private'
 const $ = 'some value'
@@ -15,12 +15,53 @@ const withNum5 = '5'
 // console.log(name, lastName)
 // console.log(isProgrammer)
 
-// 2 Мутирование
+// 2 МУТИРОВАНИЕ
 
-console.log('Имя человека: ' + name + ' , а возраст человека: ' + age)
-console.log(age.toString())
+// console.log('Имя человека: ' + name + ' , а возраст человека: ' + age)
+// console.log(age.toString())
 
 // видео просмотра время 48:45
 
-const firstName = prompt('Введите фамилию')
-alert(firstName + ' ' + name)
+// const firstName = prompt('Введите фамилию')th
+// alert(firstName + ' ' + name)
+
+// 3 ОПЕРАТОРЫ
+let currentYear = 2022
+const birthYear = 1989
+
+// const age = currentYear - birthYear
+// console.log(age)
+
+const a = 10
+const b = 5
+
+let c = 32
+// c = c + a
+c += a
+c -= a
+c *= a
+c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(currentYear++) // +1
+// console.log(currentYear--) // -1
+// console.log(c)
+
+// 4 ТИПЫ ДАННЫХ
+// const isProgrammer = true
+// const name = 'Yulia'
+// const age = 33
+// let x
+
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+// console.log(null)
+
+// 5 ПРИОРИТЕТ ОПЕРАТОРОВ
+
+// время 1:03:30

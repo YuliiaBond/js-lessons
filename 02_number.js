6a484712012a8082ecdb76688e60f34e9e8a87c9
Yuliia <bondarenko047@gmail.com>
2022-04-13 22:39:14 +0300

    7

--- 02_number.js
+++ 02_number.js
@@ -76,6 +76,3 @@ function getRandomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 // console.log(getRandomBetween(10, 42))
-
-
-// 2:21:25
---
+++ 03_string.js
@@ -0,0 +1,46 @@
+// 1  S T R I N G
+
+const name = 'Yuliia'
+const age = 33
+
+function getAge() {
+  return age
+}
+//  const output = 'Hello! I am ' + name + '. I am ' + age + '.'
+// const output = `Hello! I am ${name}. I am ${getAge()}.`
+// const output = `Hello! I am ${name}. I am ${age < 30 ? 'A' : 'B'}.`
+// console.log(output)
+
+
+// const teacher = 'Vladilen'
+// console.log(teacher.length)
+// console.log(teacher.toUpperCase())
+// console.log(teacher.toLowerCase())
+// console.log(teacher.charAt(2))
+// console.log(teacher.indexOf('!'))
+// console.log(teacher.startsWith('vlad'))
+// console.log(teacher.toLowerCase().startsWith('vlad'))
+// console.log(teacher.endsWith('en!'))
+// console.log(teacher.repeat(3))
+
+// const string = '    password    '
+// console.log(string)
+// console.log(string.trim())
+// console.log(string.trimStart())
+// console.log(string.trimEnd())
+
+function logPerson(s, name, age) {
+  console.log(s, name, age)
+  if (age > 18) {
+    age = '18+'
+  }
+  // return 'Info about person'
+  return `${s[0]}${name}${s[1]}${age}${s[2]}`
+}
+
+const personName = 'Yuliia'
+const personAge = 33
+
+const output = logPerson`Name: ${personName}, age: ${personAge}!`
+
+console.log(output)

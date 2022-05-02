d3e6b83b690791c4883d5960e207032581efaa98
Yuliia <bondarenko047@gmail.com>
2022-05-03 00:11:48 +0300

    10

---
+++ 06_object.js
@@ -0,0 +1,14 @@
+// O b j e c t
+const person = {
+  name: 'Vladilen',
+  age: 26,
+  isProgrammer: true,
+  languages: ['ru', 'en', 'de'],
+  greet() {
+    console.log('greet from person')
+  }
+}
+console.log(person)
+console.log(person.name)
+
+//3;47;58

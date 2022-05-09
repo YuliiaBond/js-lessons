1302d80110fd5cca2100567536a145c30d72cc56
Yuliia <bondarenko047@gmail.com>
2022-05-09 22:06:50 +0300

    14

---
+++ 07_async.js
@@ -0,0 +1,14 @@
+// Event Loop
+
+// const timeout = setTimeout(() => {
+//   console.log('After timeout')
+// }, 2500)
+
+// clearTimeout(timeout)
+
+// const interval = setInterval(() => {
+//   console.log('After interval')
+// }, 1000)
+
+// clearInterval(interval)
+
